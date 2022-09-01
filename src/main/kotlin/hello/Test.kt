901344ef21de527c691531137070a88544bb06e0
Severin Gehwolf <sgehwolf@redhat.com>
2022-09-01 14:46:13 +0200

    Simplify native image reproducer

--- src/main/kotlin/hello/Test.kt
+++ src/main/kotlin/hello/Test.kt
@@ -18,21 +18,11 @@ fun newVal(x: Int) {
 val KotlinHelloString : String = "Hello from Kotlin!"
 
 fun getHelloStringFromJava() : String {
-    // val g = Class.forName("hello.Test\$special\$\$inlined\$vetoable\$1").declaringClass
-    val g = Class.forName("hello.Test\$special\$\$inlined\$vetoable\$1")
-    val obj = g::class
-    println(Test::class)
-    println(obj.constructors)
-    //val f = Class.forName("hello.Test\$special\$\$inlined\$sortedByDescending\$1").declaringClass
-    println(g.getName())
-    println(g.declaringClass)
-    //println(f)
     val x = Test()
     println(x.bla::class)
     x.newVal(20)
     println(x.baz)
     x.newVal(10)
-    println(x.baz)
     return JavaHello.JavaHelloString
 }
 
