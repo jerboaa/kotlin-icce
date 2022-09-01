0b844a4a71b7f8e5bb66e23f9a08900dae42b01b
Severin Gehwolf <sgehwolf@redhat.com>
2022-09-01 14:40:50 +0200

    Make it reproduce with native-image

--- src/main/kotlin/hello/Test.kt
+++ src/main/kotlin/hello/Test.kt
@@ -6,7 +6,11 @@ class Test {
 
 val bla = listOf("bla").sortedByDescending { it }
 var baz: Int by Delegates.vetoable(0) { property, oldValue, newValue ->
-    newValue > oldValue
+    if (newValue > oldValue) true else throw IllegalArgumentException("New value must be larger than old value.")
+}
+
+fun newVal(x: Int) {
+    baz = x
 }
 
 }
@@ -14,12 +18,21 @@ var baz: Int by Delegates.vetoable(0) { property, oldValue, newValue ->
 val KotlinHelloString : String = "Hello from Kotlin!"
 
 fun getHelloStringFromJava() : String {
-    val g = Class.forName("hello.Test\$special\$\$inlined\$vetoable\$1").declaringClass
-    val f = Class.forName("hello.Test\$special\$\$inlined\$sortedByDescending\$1").declaringClass
-    println(g)
-    println(f)
+    // val g = Class.forName("hello.Test\$special\$\$inlined\$vetoable\$1").declaringClass
+    val g = Class.forName("hello.Test\$special\$\$inlined\$vetoable\$1")
+    val obj = g::class
+    println(Test::class)
+    println(obj.constructors)
+    //val f = Class.forName("hello.Test\$special\$\$inlined\$sortedByDescending\$1").declaringClass
+    println(g.getName())
+    println(g.declaringClass)
+    //println(f)
     val x = Test()
-    println(x.bla)
+    println(x.bla::class)
+    x.newVal(20)
+    println(x.baz)
+    x.newVal(10)
+    println(x.baz)
     return JavaHello.JavaHelloString
 }
 
